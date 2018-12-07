52677e582847db52b9c5b29cd680ea937e7dfb81
devendrk <katwaldevendra@hotmail.com>
2018-12-07 08:43:46 +0200

    fixed conflict

--- src/components/modals/signIn/SignIn.js
+++ src/components/modals/signIn/SignIn.js
@@ -10,76 +10,7 @@ import "./SignIn.css";
 
 Modal.setAppElement('body')
 
-<<<<<<< HEAD
-class SignIn extends Component {
-  state = {
-    isModelOpen: false
-  };
 
-  // componentDidMount() {
-  //   Modal.setAppElement('body');
-  // }
-  
-  openModel = () =>
-    this.setState({
-      isModelOpen: true
-    });
-  closeModel = () => {
-    this.setState({
-      isModelOpen: false
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        {this.state.isModelOpen ? (
-          <Modal
-            className="login-card"
-            isOpen={this.state.isModelOpen}
-            isClosed={this.state.isModelOpen}
-          >
-            <h1>Log-in</h1> <br />
-            <form name="signIn" onSubmit={this.props.handleSubmit}>
-              <Field
-                component="input"
-                type="text"
-                name="userName"
-                placeholder=" email"
-                autoComplete = "email"
-              />
-              <Field
-                component="input"
-                type="password"
-                name="password"
-                autoComplete = "password"
-                placeholder="Password"
-              />
-              <div className="btn-section">
-                <Button className="custom-btn btn-green" label="Sign In" type="submit" />
-                <Button
-                  className="custom-btn btn-red"
-                  onClick={this.closeModel}
-                  label="cancel"
-                  type="button"
-                />
-              </div>
-            </form>
-            <div className="login-help">
-              <a href="/">Register</a> • <a href="/">Forgot Password</a>
-            </div>
-          </Modal>
-        ) : (
-          <div className="bar-right-section ">
-            <Button
-              className="custom-btn "
-              label="Login"
-              type="submit"
-              onClick={this.openModel}
-            />
-          </div>
-        )}
-=======
 const SignIn = (props) => {
   const { signInValues } = props;
   return (
@@ -109,8 +40,8 @@ const SignIn = (props) => {
       </form>
       <div className="login-help">
         <p>Need help ?  <a href = "/">  Hello@mapple.fi</a></p>
->>>>>>> 9dca66dd392ce65cc381382949aa52e666041541
       </div>
+
     </div>
   );
 }
